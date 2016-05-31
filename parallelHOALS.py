43951108733443eb23fae3325c77b652384b2252
RomainWarlop <romainwarlop@gmail.com>
2016-05-31 16:27:39 +0200

    push data

--- parallelHOALS.py
+++ parallelHOALS.py
@@ -1,30 +1,139 @@
 from pyspark import SparkContext
 from pyspark.mllib.recommendation import ALS, Rating
-from sys import argv
+from pyspark.sql import SQLContext
 
-path = "/home/data/file.csv"
+sc = SparkContext("local", "HOALS core")
+sqlContext = SQLContext(sc)
 
-data = sc.textFile(path)
+import pandas as pd
+import numpy as np
+import sktensor
+import re
+import itertools
 
-data_separator = '\t'
+def HOALS(data,dims,ranks,model='tucker',lambda_=0.8,alpha=0.1,num_iters=5,implicit=False):
+    """
+    Parameters
+    data : DataFrame	
+        [0] : userId
+        [1] : itemId
+        [2] : actionId
+        [3] : rating
+    dims : list
+        [0] : number of users
+        [1] : number of items
+        [2] : number of actions
+    """
+    C_train = sktensor.sptensor((data[2],data[0],data[1]),
+	        data[3],shape=(dims[2],dims[0],dims[1]))
 
-def ALS(data,dim,rank = 5,lambda_=0.8,alpha=0.01,num_iters=20,implicit=False):
+    #==============================================================================
+    # recuparation of the (user,item,rate) of the unfold matrix
+    #==============================================================================
 
-	def getRatings(l):
-		if float(l[4])==dim:
-			return Rating(float(l[0]), float(l[1]), float(l[2]))
+    # train set
+    C1 = sktensor.csr_matrix(C_train.unfold(1))
+    y1 = list(C1.indices)
+    indptr1 = C1.indptr
+    r1 = list(C1.data)
+    tmp1 = indptr1[1:len(indptr1)]-indptr1[0:(len(indptr1)-1)]
+    x1 = []
+    for i in np.arange(len(tmp1)):
+        x1.extend(np.repeat(i,tmp1[i]))
 
-	ratings = data.map(lambda l: l.split(data_separator)).map(getRatings)
+    C2 = sktensor.csr_matrix(C_train.unfold(2))
+    y2 = list(C2.indices)
+    indptr2 = C2.indptr
+    r2 = list(C2.data)
+    tmp2 = indptr2[1:len(indptr2)]-indptr2[0:(len(indptr2)-1)]
+    x2 = []
+    for i in np.arange(len(tmp2)):
+        x2.extend(np.repeat(i,tmp2[i]))
+        
+    C3 = sktensor.csr_matrix(C_train.unfold(0))
+    y3 = list(C3.indices)
+    indptr3 = C3.indptr
+    r3 = list(C3.data)
+    tmp3 = indptr3[1:len(indptr3)]-indptr3[0:(len(indptr3)-1)]
+    x3 = []
+    for i in np.arange(len(tmp3)):
+        x3.extend(np.repeat(i,tmp3[i]))
 
-	if implicit:
-		model = ALS.trainImplicit(ratings=ratings, rank=rank, iterations=num_iters, seed=0, lambda_=lambda_, alpha=apha)
-	else:
-		model = ALS.train(ratings=ratings, rank=rank, iterations=num_iters, seed=0, lambda_=lambda_)
+    dataTrain = {}
+    dataTrain[0] = pd.DataFrame([x1,y1,r1]).T
+    dataTrain[1] = pd.DataFrame([x2,y2,r2]).T
+    dataTrain[2] = pd.DataFrame([x3,y3,r3]).T
+    
+    dataTrain[0] = dataTrain[0][dataTrain[0][2]!=0] # where the rating is not null
+    dataTrain[1] = dataTrain[1][dataTrain[1][2]!=0]
+    dataTrain[2] = dataTrain[2][dataTrain[2][2]!=0]
 
-	features = model.userFeatures()
-	return features
+    #==============================================================================
+    # Factorization
+    #==============================================================================
+    ratings = {}
+    res = {}
+    features = {}
+    for i in range(3):
+        if i==0:
+            mode = 'User'
+        elif i==1:
+            mode = 'Item'
+        elif i==2:
+            mode = 'Action'
 
-def HOALS(data,rank = [5,5,5],lambda_=0.8,alpha=0.01,num_iters=20,implicit=False):
+        print("Start "+mode+" Learning")
+        
+        dataTrain[i] = sqlContext.createDataFrame(dataTrain[i]).rdd
+        ratings[i] = dataTrain[i].map(lambda l: Rating(float(l[0]), float(l[1]), float(l[2])))
 
-	nDim = len(a)
+      # Build the recommendation model using Alternating Least Squares
+
+        if implicit:
+            res[i] = ALS.trainImplicit(ratings=ratings[i], rank=ranks[i], iterations=num_iters, seed=0, lambda_=lambda_, alpha=alpha)
+        else:
+            res[i] = ALS.train(ratings=ratings[i], rank=ranks[i], iterations=num_iters, seed=0, lambda_=lambda_)
+
+        features[i] = res[i].userFeatures()
 	
+    #==============================================================================
+    # Post processing -> export to classic python
+    #==============================================================================
+    A = {}
+    A_star = {}
+    for j in range(3):
+        features[j] = sqlContext.createDataFrame(features[j]).toPandas()
+        A[j] = []
+        for i in range(features[j].shape[0]):
+            a = features[j].ix[i][1]
+            A[j].append(a)
+        A[j] = np.array(A[j])
+        A_star[j] = np.linalg.pinv(A[j])
+
+    if model=="tucker":
+        if implicit:
+            tmp = sktensor.sptensor((data[2],data[0],data[1]),list(np.repeat(1,len(data))),
+                                        shape=(dims[2],dims[0],dims[1]))
+            W = tmp.ttm(A_star[0],mode=1).ttm(A_star[1],mode=2).ttm(A_star[2],mode=0)
+        else:
+            W = C_train.ttm(A_star[0],mode=1).ttm(A_star[1],mode=2).ttm(A_star[2],mode=0)
+
+        #C_hat = W.ttm(A[0],mode=1).ttm(A[1],mode=2).ttm(A[2],mode=0) # to heavy
+        def C_hat(a,u,i):
+            out = 0
+            P = [range(ranks[i]) for i in range(len(ranks))]
+            for elt in itertools.product(*P):
+                m,l,s = elt
+                out += W[s,m,l]*A[0][u,m]*A[1][i,l]*A[2][a,s]
+            
+            return out
+    elif model=="cp":
+        #C_hat = sktensor.ktensor([A[2],A[0],A[1]]).totensor() # to heavy
+        def C_hat(a,u,i):
+            out = 0
+            for r in np.arange(ranks[0]):
+                out += A[0][u,r]*A[1][i,r]*A[2][a,r]
+            
+            return out
+
+    return C_hat
---
+++ testHOALS.py
@@ -0,0 +1,180 @@
+from pyspark import SparkContext
+from parallelHOALS import HOALS
+
+from random import shuffle
+import numpy as np
+import pandas as pd
+import itertools
+
+def timespend(x):
+    if x<60:
+        return str(x)+' sec'
+    elif x<3600:
+        m, s = divmod(x,60)
+        m, s = int(m), int(s)
+        return str(m)+' min '+str(s)+' sec'
+    else:
+        m, s = divmod(x,60)
+        h, m = divmod(m,60)
+        h, m = int(h), int(m)
+        return str(h)+' hour '+str(m)+' min '+str(s)+' sec'
+
+# fix all the random seeds
+np.random.seed(0)
+
+path = "/home/romain/Documents/Github/HOALS/"
+data = pd.read_csv(path+"3S-70k.csv", sep = ";")
+
+selectedItem = np.unique(data['codssfamille'])
+# filter out item which have been seen less than 5 times
+itemCount = [np.sum(data['codssfamille']==i) 
+    for i in selectedItem]
+
+selectedItem = [selectedItem[i] for i in np.arange(len(selectedItem)) 
+                    if itemCount[i]>5]
+
+userId = pd.DataFrame({'fullVisitorId': np.unique(data['fullVisitorId']),
+                       'userId': np.arange(len(np.unique(data['fullVisitorId'])))})
+                       
+catId = pd.DataFrame({'codssfamille': selectedItem,
+                       'catId': np.arange(len(selectedItem))})
+
+data2 = pd.merge(pd.merge(data, userId), catId)
+
+numUser = userId.shape[0]
+numItem = catId.shape[0]
+
+shuffleUserId = np.arange(numUser)
+shuffle(shuffleUserId)
+shuffleUserId = list(shuffleUserId)
+
+# weight
+data2['nPPview'][data2['nPPview']>50] = 50.0
+maxClick = float(np.max(data2['nPPview']))/10
+
+data2['nATC'][data2['nATC']>20] = 20.0
+maxATC = float(np.max(data2['nATC']))/10
+
+data2['nConv'][data2['nConv']>10] = 10.0
+maxConv = float(np.max(data2['nConv']))/10
+
+# K cross validation
+K = 5
+KshuffleUserId = []
+start = 0
+end = int(numUser/K)
+L = int(numUser/K)
+for k in range(K):
+    if k!=(K-1):
+        KshuffleUserId.append(shuffleUserId[start:end])
+        start = end
+        end+=L
+    else:
+        KshuffleUserId.append(shuffleUserId[start:])
+
+lambdas = [0.01,1.0,10.0]
+alphas = [0.01,1.0,10.0]
+for k in [0]:
+    print('*'*20)
+    print('start cross-val',k)
+    print('*'*20)
+
+    trainUser = []
+    testUser = []
+    for i in range(K):
+        if i==k:
+            testUser = KshuffleUserId[i]
+        else:
+            trainUser.extend(KshuffleUserId[i])
+
+    split = [1 if ((data2['userId'][i] in trainUser) | 
+            ((data2['userId'][i] not in trainUser) & (np.random.random()<0.7)))
+                else 0 
+               for i in range(data2.shape[0])]
+
+
+    xyrTrain = [(data2['userId'][i],data2['catId'][i],data2['nPPview'][i]/maxClick,
+                 data2['nATC'][i]/maxATC,data2['nConv'][i]/maxConv) 
+                 for i in range(data2.shape[0])
+                 if split[i]==1]
+
+    xyrTest = [(data2['userId'][i],data2['catId'][i],data2['nPPview'][i]/maxClick,
+                data2['nATC'][i]/maxATC,data2['nConv'][i]/maxConv) 
+                for i in range(data2.shape[0])
+                if split[i]==0]
+    
+    # filling training set
+    z_train = list(np.repeat([0,1,2],len(xyrTrain)))
+    z_train = list(map(int,z_train))
+
+    xuser_train = [x for (x,y,r1,r2,r3) in xyrTrain] 
+    xuser_train = xuser_train + xuser_train + xuser_train # click + atc + conv
+
+    yitem_train = [y for (x,y,r1,r2,r3) in xyrTrain] 
+    yitem_train = yitem_train + yitem_train + yitem_train # click + atc + conv
+
+    r_train = [float(r1) for (x,y,r1,r2,r3) in xyrTrain] # click
+    r_train.extend([float(r2) for (x,y,r1,r2,r3) in xyrTrain]) # atc
+    r_train.extend([float(r3) for (x,y,r1,r2,r3) in xyrTrain]) # conv
+
+    # filling test set
+    z_test = list(np.repeat([0,1,2],len(xyrTest)))
+    z_test = list(map(int,z_test))
+
+    xuser_test = [x for (x,y,r1,r2,r3) in xyrTest]
+    xuser_test = xuser_test + xuser_test + xuser_test # click + atc + conv
+
+    yitem_test = [y for (x,y,r1,r2,r3) in xyrTest] 
+    yitem_test = yitem_test + yitem_test + yitem_test # click + atc + conv
+
+    r_test = [float(r1) for (x,y,r1,r2,r3) in xyrTest] # click
+    r_test.extend([float(r2) for (x,y,r1,r2,r3) in xyrTest]) # atc
+    r_test.extend([float(r3) for (x,y,r1,r2,r3) in xyrTest]) # conv
+
+    inputTrainingSetTensor = pd.DataFrame([xuser_train,yitem_train,z_train,r_train]).T
+    inputTestSetTensor = pd.DataFrame([xuser_test,yitem_test,z_test,r_test]).T
+
+    for i in range(3):
+        inputTrainingSetTensor[i] = list(map(int,inputTrainingSetTensor[i]))
+        inputTestSetTensor[i] = list(map(int,inputTestSetTensor[i]))
+
+    for elt in itertools.product(*[lambdas,alphas]):
+        lambda_, alpha = elt
+        print('lambda :',lambda_)
+        print('alpha :',alpha)
+
+        import time
+        t0 = time.time()
+        C_hat = HOALS(data = inputTrainingSetTensor, dims = [numUser,numItem,3], ranks=[10,200,2], model = 'tucker',
+                      lambda_ = lambda_, alpha = alpha, num_iters = 20, implicit = True)
+        t1 = time.time()
+        print('-'*20)
+        print('executation time :',timespend(t1-t0))
+        print('-'*20)
+
+        rhat = []
+        for i in np.arange(len(r_test)):
+            rhat_i = C_hat(z_test[i],xuser_test[i],yitem_test[i])
+            rhat.extend([rhat_i])
+        rhat = pd.Series(rhat)
+        
+        numAction = 3
+        num = np.zeros(numAction)
+        den = np.zeros(numAction)
+        for u in range(numUser):
+            for a in range(numAction):
+                delta = int(len(z_test)/numAction) 
+                ind = (np.where(np.array(xuser_test[a*delta:(a+1)*delta])==u)[0]+a*delta).tolist()
+                r1 = list(np.array(r_test)[ind])
+                r2 = rhat[ind]
+                tmp = list(-np.sort(-r2))
+                r = [tmp.index(list(r2)[i])/float(len(r1))*r1[i]
+                        for i in np.arange(len(r1))]
+                r = 100*np.round(sum(r),2)
+                num[a]+=r
+                den[a]+=np.sum(r1)
+
+        rank = num/den
+        print('*'*20)
+        print('rank-measure :',rank)
+        print('*'*20)
